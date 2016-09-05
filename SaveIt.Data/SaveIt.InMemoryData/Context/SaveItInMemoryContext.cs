a88e6c07fb2c5b91284b98c28582bc6cde776ce4
coseguera <coseguera@users.noreply.github.com>
2016-09-04 20:37:59 -0700

    implement more in-memory entities

--- SaveIt.Data/SaveIt.InMemoryData/Context/SaveItInMemoryContext.cs
+++ SaveIt.Data/SaveIt.InMemoryData/Context/SaveItInMemoryContext.cs
@@ -10,17 +10,38 @@ namespace SaveIt.InMemoryData.Context
         {
             this.Users = new List<SaveItUser>();
             this.Accounts = new List<Account>();
+            this.Categories = new List<Category>();
+            this.People = new List<Person>();
+            this.AccountTransactions = new List<AccountTransaction>();
         }
 
         public List<SaveItUser> Users { get; private set; }
 
         public List<Account> Accounts { get; private set; }
 
+        public List<Category> Categories { get; private set; }
+
+        public List<Person> People { get; private set; }
+
+        public List<AccountTransaction> AccountTransactions { get; private set; }
+
         public List<T> GetEntitySet<T>() where T : SaveItEntityBase
         {
-            if(typeof(T) == typeof(Account))
+            if (typeof(T) == typeof(Account))
             {
                 return Accounts as List<T>;
+            }
+            if (typeof(T) == typeof(Category))
+            {
+                return Categories as List<T>;
+            }
+            if (typeof(T) == typeof(Person))
+            {
+                return People as List<T>;
+            }
+            if (typeof(T) == typeof(AccountTransaction))
+            {
+                return AccountTransactions as List<T>;
             }
 
             throw new NotImplementedException();
